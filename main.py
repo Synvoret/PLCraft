7b922c7bcc79e645a451397aab33b80949e746a0
Lukasz Szabat <synvoret@gmail.com>
2023-04-21 01:57:16 +0200

    git add .

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, render_template, url_for, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    made_used = os.listdir('static\img\made-used')
+    print(made_used)
+    return render_template('index.html', made_used=made_used)
+
+if __name__ == '__main__':
+    app.run(debug=True)
