f5951c9826b127dd3e1bb3770d723c2e5e176809
synvoret <synvoret@gmail.com>
2025-05-20 09:11:42 +0200

    refresh

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    made_used = os.listdir('static\img\made-used')
+    made_used = os.listdir(r'static/img/made-used')
     print(made_used)
     return render_template('index.html', made_used=made_used)
 
